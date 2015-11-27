84bf66747a0f3dcfe88209ecfc52eda9f14834fb
Naoki Sato <naokis@live.com>
2015-11-27 18:20:31 +0900

    1st release

---
+++ SpeechRecognition/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace SpeechRecognition
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            try {
+                if (e.Parameter.ToString() == "Red")
+                    Container.Background = new SolidColorBrush(Windows.UI.Colors.DarkRed);
+            }
+            catch { }
+
+            
+            base.OnNavigatedTo(e);
+        }
+    }
+
+}
---
+++ VoiceCommandService/HolVoiceCommandService.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Windows.ApplicationModel.Background;
+using Windows.ApplicationModel.AppService;
+using Windows.ApplicationModel.VoiceCommands;
+
+
+namespace VoiceCommandService
+{
+    public sealed class HolVoiceCommandService : IBackgroundTask
+    {
+        VoiceCommandServiceConnection voiceServiceConnection;
+
+        BackgroundTaskDeferral serviceDeferral;
+
+
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            serviceDeferral = taskInstance.GetDeferral();
+
+            taskInstance.Canceled += OnTaskCanceled;
+
+            var triggerDetails = taskInstance.TriggerDetails as AppServiceTriggerDetails;
+
+            if (triggerDetails != null && triggerDetails.Name == "HolVoiceCommandService")
+            {
+                try
+                {
+                    voiceServiceConnection =
+                                    VoiceCommandServiceConnection.FromAppServiceTriggerDetails(
+                                        triggerDetails);
+
+                    voiceServiceConnection.VoiceCommandCompleted += OnVoiceCommandCompleted;
+
+
+                    VoiceCommand voiceCommand = await voiceServiceConnection.GetVoiceCommandAsync();
+
+                    switch (voiceCommand.CommandName)
+                    {
+                        case "SayHello":
+
+                            var userMessage = new VoiceCommandUserMessage();
+                            userMessage.DisplayMessage = "お店で合言葉話してね。";
+                            userMessage.SpokenMessage = "ごきげんよう。";
+
+                            var response = VoiceCommandResponse.CreateResponse(userMessage);
+
+                            await voiceServiceConnection.ReportSuccessAsync(response);
+
+                            break;
+
+
+                        default:
+                            break;
+                    }
+
+
+                }
+                catch (Exception ex)
+                {
+                    System.Diagnostics.Debug.WriteLine("Handling Voice Command failed " + ex.ToString());
+                }
+            }
+
+
+        }
+
+
+        private void OnVoiceCommandCompleted(VoiceCommandServiceConnection sender, VoiceCommandCompletedEventArgs args)
+        {
+            if (this.serviceDeferral != null)
+            {
+                this.serviceDeferral.Complete();
+            }
+        }
+
+        private void OnTaskCanceled(IBackgroundTaskInstance sender, BackgroundTaskCancellationReason reason)
+        {
+            System.Diagnostics.Debug.WriteLine("Task cancelled, clean up");
+            if (this.serviceDeferral != null)
+            {
+                //Complete the service deferral
+                this.serviceDeferral.Complete();
+            }
+        }
+
+
+
+
+    }
+}
